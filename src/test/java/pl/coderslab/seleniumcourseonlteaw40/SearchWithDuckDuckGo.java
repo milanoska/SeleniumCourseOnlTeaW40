665cad577f206584c8bb52beb6f43a4d5063fc6f
Miro <mirek31@gmail.com>
2023-07-23 14:31:30 +0200

    D4 Search

---
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw40/SearchWithDuckDuckGo.java
@@ -0,0 +1,22 @@
+package pl.coderslab.seleniumcourseonlteaw40;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SearchWithDuckDuckGo {
+    @Test
+    public void searchWithDuckDuckGo() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://duckduckgo.com/");
+        WebElement searchInputBox = driver.findElement(By.id("search_form_input_homepage"));
+        searchInputBox.sendKeys("w pustyni i w puszczy");
+        WebElement searchButton = driver.findElement(By.id("search_button_homepage"));
+        searchButton.click();
+        WebElement clickFirstRecord = driver.findElement(By.id("r1-0"));
+        clickFirstRecord.click();
+        //driver.quit();
+    }
+}
