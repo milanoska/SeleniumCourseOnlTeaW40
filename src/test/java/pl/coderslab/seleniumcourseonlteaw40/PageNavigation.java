63c8e751eab49ba5edaa6d98cfc39c882196024f
Miro <mirek31@gmail.com>
2023-07-23 15:12:42 +0200

    D4 PageNavigation Test

--- src/test/java/pl/coderslab/seleniumcourseonlteaw40/PageNavigation.java
+++ src/test/java/pl/coderslab/seleniumcourseonlteaw40/PageNavigation.java
@@ -1,18 +1,16 @@
 package pl.coderslab.seleniumcourseonlteaw40;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class PageNavigation {
+public class PageNavigationTest {
     @Test
     public void searchWithDuckDuckGo() {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://duckduckgo.com/");
-        driver.get("https://duckduckgo.com/");
-        driver.get("https://duckduckgo.com/");
+        driver.get("https://coderslab.pl/pl");
+        driver.get("https://mystore-testlab.coderslab.pl/index.php");
+        driver.get("https://hotel-testlab.coderslab.pl/en/");
         //driver.quit();
     }
 }
